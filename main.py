6ea9d86bc0949673d4c2859ff3c28de702dd2ff6
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-06-29 01:21:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+count = 0
+for n in student_heights:
+  sum += n
+  count += 1
+sum = sum / count
+
+print(f"Average height is: {round(sum)}")
+
